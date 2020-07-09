5b7bef15634d82621d33f4de12c4be220fe7a585
shaikhzeshan@gmail.com <shaikhzeshan@gmail.com>
2020-07-09 12:42:15 -0500

    redux reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,68 @@
+import {ADD_RECIPE, REMOVE_FROM_CALENDER} from '../actions/index';
+
+const initialCalendarState = {
+    sunday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    monday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    tuesday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    wednesday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    thursday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    friday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+    saturday: {
+      breakfast: null,
+      lunch: null,
+      dinner: null,
+    },
+  }
+
+  function calender (state = initialCalendarState, action){
+      const {day, meal, recipe} = action
+
+      switch(action.type){
+          case 'ADD_RECIPE':
+              return {
+                  ...state,
+                     [day]:{
+                      ...state[day],
+                      [meal]:recipe.label,
+
+                  }
+              }
+          case  'REMOVE_FROM_CALENDER':
+              return {
+                  ...state,
+                  [day]:{
+                    ...state[day],
+                    [meal]:null
+                  }
+
+              }
+              
+
+
+  }
+
+  export default calender;
